a33134f04842928d87b1c8ead69e68831820f0e0
Nagesh Patil <nageshvkadam21@gmail.com>
2026-09-23 12:47:31 +0530

    Some Change

---
+++ Programs/A.java
@@ -0,0 +1,23 @@
+class A{
+
+    void voting( int age){
+    //Apply The A If A condition
+       // age;
+    if(age>18){
+        System.out.println("Eligible For A Vote ");
+    }
+
+    else
+    {
+        System.out.println("Not ");
+    }
+
+    
+}
+
+public static void main(String[] args) {
+    //create A Objet 
+    A b=new A();
+    b.voting(45);
+}
+}
